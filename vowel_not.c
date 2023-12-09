f5bf6b37c74695b3f4efbd373eaf6dd74f31ad85
Raghav <146563913+rghvsrdhtra@users.noreply.github.com>
2023-12-09 19:56:49 +0530

    vowel_not.c

--- vowel_not.c
+++ vowel_not.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
-int main()
-{
+
+int main() {
     char character;
     printf("Enter a character: ");
-    scanf("%c", character);
+    scanf("%c", &character); 
 
     (character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u' || 
-    character == 'A' || character == 'E' || character == 'I' || character == 'O' || character == 'U') ? printf ("Given Character is a is a vowel") : printf("Given Character is a consonant");
+    character == 'A' || character == 'E' || character == 'I' || character == 'O' || character == 'U') ? 
+    printf("Given Character is a vowel\n") : printf("Given Character is a consonant\n");
+
     return 0;
 }
